7d16f89d1ce2b73570ade4b56a590fb3158b8b8c
Sempanha123 <sempanha02@gmail.com>
2024-06-07 15:18:28 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {},
+      theme: {
+        container: {
+          center: true,
+          padding: '1rem',
+          screens: {
+            sm: '600px',
+            md: '728px',
+            lg: '984px',
+            xl: '1240px',
+            '2xl': '1240px',
+          },
+          
+        },
+      },
     },
     plugins: [],
   }
