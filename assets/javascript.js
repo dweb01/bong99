fb72b5d1c40da6cfa4557f43e30f1c6ac17e8e40
Sempanha123 <sempanha02@gmail.com>
2024-06-07 14:30:29 +0700

    update

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,16 +1,16 @@
-// var btn = $(".Btn-backtotop1");
-// $(window).scroll(function () {
-//     if ($(window).scrollTop() > 300) {
-//       btn.addClass("show");
-//     } else {
-//       btn.removeClass("show");
-//     }
-//   });
+var btn = $(".Btn-backtotop1");
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass("show");
+    } else {
+      btn.removeClass("show");
+    }
+  });
   
-//   btn.on("click", function (e) {
-//     e.preventDefault();
-//     $("html, body").animate({ scrollTop: 0 }, "300");
-//   });
+  btn.on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "300");
+  });
   
 
 
